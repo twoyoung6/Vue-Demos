d2ccb50ea1909a657dbbe0b1419c168ec71c807a
Twoyoung <7886903+twoyoung@user.noreply.gitee.com>
2023-09-12 16:34:00 +0800

    前端工程化之使用 webpack-spritesmith 自动合成精灵图

---
+++ src/views/Sprite.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sprite">
+    <div class="ico ico-a"></div>
+    <div class="ico ico-b"></div>
+    <div class="ico ico-c"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/sprite/sprite.scss';
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
 const path = require('path')
 const vConsolePlugin = require('vconsole-webpack-plugin')
 const resolve = dir => path.join(__dirname, dir)
+
+const SpritesmithPlugin = require('webpack-spritesmith');
+const templateFunction = function (data) {
+  const shared = '.ico { background: url(I)  no-repeat; background-size: Wpx Hpx;}'
+    .replace('I', data.sprites[0].image)
+    .replace('W', data.sprites[0].total_width)
+    .replace('H', data.sprites[0].total_height)
+  const perSprite = data.sprites.map(function (sprite) {
+    return '.ico-N { width: Wpx; height: Hpx; background-position: Xpx Ypx; }'
+      .replace('N', sprite.name)
+      .replace('W', sprite.width)
+      .replace('H', sprite.height)
+      .replace('X', sprite.offset_x)
+      .replace('Y', sprite.offset_y);
+  }).join('\n');
+
+  return shared + '\n' + perSprite;
+}
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: config => {
@@ -15,7 +34,31 @@ module.exports = {
         enable: debug
       })
     ]
-    config.plugins = [...config.plugins, ...pluginsDev]
+    // 合成精灵图配置
+    config.resolve.modules = ["node_modules", "sprite"]
+    let spritePlugins = [
+      new SpritesmithPlugin({
+        src: {
+          cwd: path.resolve(__dirname, 'src/assets/icons'),
+          glob: '*.png'
+        },
+        target: {
+          image: path.resolve(__dirname, 'src/sprite/sprite.png'),
+          css: [
+            [path.resolve(__dirname, 'src/sprite/sprite.scss'), {
+              format: 'handlebars_based_template'
+            }]
+          ]
+        },
+        customTemplates: {
+          'handlebars_based_template': templateFunction,
+        },
+        apiOptions: {
+          cssImageRef: "~sprite.png"
+        }
+      })
+    ]
+    config.plugins = [...config.plugins, ...pluginsDev, ...spritePlugins]
   },
 
   chainWebpack: config => {
